d587a62c9e2dc360ed257882230530f342727541
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-10-12 19:20:40 -0400

    app.js routing

--- bigleaf/src/App.js
+++ bigleaf/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import WhoWeAre from './components/WhoWeAre';
+import Contact from './components/Contact';
+import TreeCare from './components/TreeCare';
+import './App.scss';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter basename='/'>
+      <Routes>
+          <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<WhoWeAre />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/treecare" element={<TreeCare />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   );
 }
 
