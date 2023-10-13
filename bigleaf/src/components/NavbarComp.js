4d7181bba9f9fa781aa2f339f83a6885ab11f6b9
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-10-13 17:34:45 -0400

    had to reorder the drop down for service area

--- bigleaf/src/components/NavbarComp.js
+++ bigleaf/src/components/NavbarComp.js
@@ -36,23 +36,23 @@ function NavbarComp() {
             <NavDropdown title="Service Areas" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1"></NavDropdown.Item>
               <NavDropdown title="Service Area" id="servArea-menu">
-              <NavDropdown.Item href="#action/3.1.2">Acworth</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1.3">Alpharetta</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1.1">Atlanta</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.2">Brookhaven</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.3">Canton</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.4">Cumming</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.5">Dunwoody</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.3">Gainesville</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.3">Johns Creek</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.3">Kennesaw</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.3">Marietta</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.3">McDonough</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.3">Milton</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.3">Newnan</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.3">Smyrna</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.3">Roswell</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.3">Woodstock</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1.1">Acworth</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1.2">Alpharetta</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1.3">Atlanta</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1.4">Brookhaven</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1.5">Canton</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1.6">Cumming</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1.7">Dunwoody</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1.8">Gainesville</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1.9">Johns Creek</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1.10">Kennesaw</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1.11">Marietta</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1.12">McDonough</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1.13">Milton</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1.14">Newnan</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1.15">Smyrna</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1.16">Roswell</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1.17">Woodstock</NavDropdown.Item>
             {/* need to find out the areas we cover */}
                 </NavDropdown> 
               {/* need another drop down for service area */}
