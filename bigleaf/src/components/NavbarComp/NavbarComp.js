9c9ceab8adfcd49d7c84e05f9a9d8a0f17254d51
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-10-20 14:35:26 -0400

    Navbar routing done

--- bigleaf/src/components/NavbarComp/NavbarComp.js
+++ bigleaf/src/components/NavbarComp/NavbarComp.js
@@ -1,8 +1,4 @@
 import React from 'react'
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 // import { Form, FormControl, Button } from 'react-bootstrap';
@@ -10,86 +6,84 @@ import './index.scss';
 
 function NavbarComp() {
   return (
-    <div className= "nav-bar">
+    <div className="nav-bar">
 
-    <Navbar  expand="lg" className="bg-body-tertiary">
-      {/* navbar to be dark is  bg="dark" get rid of classname if you want dark */}
-      <Container>
-      <Link to="/" className="navbar-brand">Bigleaf Arbor</Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-          <NavDropdown title="Tree Care" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Tree Removal</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.2">Structural Pruning</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.3">Crane Work</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Emergency Work</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.5">Stump Grinding</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Special Services" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Ivy Removal</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.2">Shrub Pruning</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Consulting" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Diagnosis</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.2">Hazard Evaluation</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Service Areas" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1"></NavDropdown.Item>
-              <NavDropdown title="Service Area" id="servArea-menu">
-              <NavDropdown.Item href="#action/3.1.1">Acworth</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1.2">Alpharetta</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1.3">Atlanta</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.4">Brookhaven</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.5">Canton</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.6">Cumming</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.7">Dunwoody</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.8">Gainesville</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.9">Johns Creek</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.10">Kennesaw</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.11">Marietta</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.12">McDonough</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.13">Milton</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.14">Newnan</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.15">Smyrna</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.16">Roswell</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1.17">Woodstock</NavDropdown.Item>
-            {/* need to find out the areas we cover */}
-                </NavDropdown> 
-              {/* need another drop down for service area */}
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.2">Residential Clients</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.3">Commercial Properties</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Institutional Clients</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Who We Are" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Meet The Team</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.2">Highlights</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.2">Clients Reviews</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Contact" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Estimate</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.2">Contact Us</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.2">Join Our Team</NavDropdown.Item>
-            </NavDropdown>
-            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-   
+      <Navbar expand="md" className="bg-body-tertiary">
+        {/* navbar to be dark is  bg="dark" get rid of classname if you want dark */}
+        <Container>
+          <Link to="/" className="navbar-brand">Bigleaf Arbor</Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavDropdown title={<Link to="/treecare">Tree Care</Link>} id="basic-nav-dropdown">
+                <NavDropdown.Item href="/treeremoval">Tree Removal</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/structuralpruning">Structural Pruning</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/cranework">Crane Work</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="emergencywork">Emergency Work</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="stumpgrinding">Stump Grinding</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title={<Link to="/specialservices">Special Services</Link>} id="basic-nav-dropdown">
+                <NavDropdown.Item href="/ivyremoval">Ivy Removal</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/shrubpruning">Shrub Pruning</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title={<Link to="/consulting">Consulting</Link>} id="basic-nav-dropdown">
+                <NavDropdown.Item href="/diagnosis">Diagnosis</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="hazardevaluation">Hazard Evaluation</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title={<Link to="/serviceareas">Service Areas</Link>} id="basic-nav-dropdown">
+                <NavDropdown title="Service Area" id="servArea-menu">
+                  <NavDropdown.Item>Acworth</NavDropdown.Item>
+                  <NavDropdown.Item>Alpharetta</NavDropdown.Item>
+                  <NavDropdown.Item>Atlanta</NavDropdown.Item>
+                  <NavDropdown.Item>Brookhaven</NavDropdown.Item>
+                  <NavDropdown.Item>Canton</NavDropdown.Item>
+                  <NavDropdown.Item>Cumming</NavDropdown.Item>
+                  <NavDropdown.Item>Dunwoody</NavDropdown.Item>
+                  <NavDropdown.Item>Gainesville</NavDropdown.Item>
+                  <NavDropdown.Item>Johns Creek</NavDropdown.Item>
+                  <NavDropdown.Item >Kennesaw</NavDropdown.Item>
+                  <NavDropdown.Item >Marietta</NavDropdown.Item>
+                  <NavDropdown.Item >McDonough</NavDropdown.Item>
+                  <NavDropdown.Item >Milton</NavDropdown.Item>
+                  <NavDropdown.Item >Newnan</NavDropdown.Item>
+                  <NavDropdown.Item >Smyrna</NavDropdown.Item>
+                  <NavDropdown.Item >Roswell</NavDropdown.Item>
+                  <NavDropdown.Item >Woodstock</NavDropdown.Item>
+                  {/* need to find out the areas we cover */}
+                </NavDropdown>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/residentialclients">Residential Clients</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/commercialproperties">Commercial Properties</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/institutionalclients">Institutional Clients</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title={<Link to="/whoweare">Who We Are</Link>} id="basic-nav-dropdown">
+                <NavDropdown.Item href="/meettheteam">Meet The Team</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/highlights">Highlights</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="clientreviews">Clients Reviews</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title={<Link to="/contact">Contact</Link>} id="basic-nav-dropdown">
+                {/* <NavDropdown title={<Link to="/contact">Contact</Link>} id="basic-nav-dropdown"> */}
+                <NavDropdown.Item href="/estimate">Estimate</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/contactus">Contact Us</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/joinourteam">Join Our Team</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
     </div>
   );
 }
